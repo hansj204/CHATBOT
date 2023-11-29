2101ff8ef4fe00ab175e3670e2bec37a63685fa2
hansj <hansj2041@gmail.com>
2023-11-29 17:22:59 +0900

    upgrade chat function

--- chatbot/mbti_chatbot/models.py
+++ chatbot/mbti_chatbot/models.py
@@ -14,24 +14,29 @@ def preprocess_sentence(sentence):
 def evaluate(sentence):
     sentence = preprocess_sentence(sentence)
     sentence = tf.expand_dims(START_TOKEN + tokenizer.encode(sentence) + END_TOKEN, axis=0)
+
     output = tf.expand_dims(START_TOKEN, 0)
 
     for i in range(MAX_LENGTH):
         predictions = model(inputs=[sentence, output], training=False)
-
         predictions = predictions[:, -1:, :]
         predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
 
         if tf.equal(predicted_id, END_TOKEN[0]):
             break
 
-    output = tf.concat([output, predicted_id], axis=-1)
-
+        output = tf.concat([output, predicted_id], axis=-1)
+        
     return tf.squeeze(output, axis=0)
 
 # 응답 메시지 생성
 def predict(sentence):
     prediction = evaluate(sentence)
     predicted_sentence = tokenizer.decode([i for i in prediction if i < tokenizer.vocab_size])
+
+    print('Input: {}'.format(sentence))
+    print('Output: {}'.format(predicted_sentence))
+
     return predicted_sentence
+
 
--- chatbot/mbti_chatbot/talkModel/talk_model.py
+++ chatbot/mbti_chatbot/talkModel/talk_model.py
@@ -5,38 +5,20 @@ import tensorflow as tf
 import os
 from .classes import *
 from .transformer import transformer
+import pickle
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
 data_folder_path = os.path.join(current_directory, 'data')
 
-train_file_path = os.path.join(data_folder_path, 'talk_training_data.csv')
+tokenizer_path = os.path.join(data_folder_path, 'tokenizer.pkl')
 model_weights_path = os.path.join(data_folder_path, 'talk_model_weights.h5')
 
-# 1. 데이터 로드하기
-train_data = pd.read_csv(train_file_path)
-train_data.head()
-
-# 2. 단어 집합 생성
-questions = [] 
-answers = []
-MAX_LENGTH = 40
-
-for sentence in train_data['Q']:
-    sentence = re.sub(r"([?.!,])", r" \1 ", sentence)
-    sentence = sentence.strip()
-    questions.append(sentence)
+with open(tokenizer_path, 'rb') as f:
+    tokenizer = pickle.load(f)
     
-
-for sentence in train_data['A']:
-    sentence = re.sub(r"([?.!,])", r" \1 ", sentence)
-    sentence = sentence.strip()
-    answers.append(sentence)
-    
-tokenizer = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(questions + answers, target_vocab_size=2**13)
-
 START_TOKEN, END_TOKEN = [tokenizer.vocab_size], [tokenizer.vocab_size + 1]
-
 VOCAB_SIZE = tokenizer.vocab_size + 2
+MAX_LENGTH = 40
 
 # 3. 트랜스포머 생성 & 가중치 로드
 D_MODEL = 256 #512

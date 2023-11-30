4c4caa90ca13ba9fdff9056411acf76f143f7518
hansj <hansj2041@gmail.com>
2023-11-30 09:28:59 +0900

    modify chat save function

--- chatbot/mbti_chatbot/models.py
+++ chatbot/mbti_chatbot/models.py
@@ -1,35 +1,5 @@
-import re
-from .talkModel.talk_model import *
+from .talkModel.talk_model import tokenizer, evaluate
 
-model = transformer_model()
-model.load_weights(model_weights_path)
-
-# 사용자 메세지 전처리
-def preprocess_sentence(sentence):
-    sentence = re.sub(r"([?.!,])", r" \1 ", sentence)
-    sentence = sentence.strip()
-    return sentence
-
-# 응답 메세지에 해당하는 단어 예측
-def evaluate(sentence):
-    sentence = preprocess_sentence(sentence)
-    sentence = tf.expand_dims(START_TOKEN + tokenizer.encode(sentence) + END_TOKEN, axis=0)
-
-    output = tf.expand_dims(START_TOKEN, 0)
-
-    for i in range(MAX_LENGTH):
-        predictions = model(inputs=[sentence, output], training=False)
-        predictions = predictions[:, -1:, :]
-        predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
-
-        if tf.equal(predicted_id, END_TOKEN[0]):
-            break
-
-        output = tf.concat([output, predicted_id], axis=-1)
-        
-    return tf.squeeze(output, axis=0)
-
-# 응답 메시지 생성
 def predict(sentence):
     prediction = evaluate(sentence)
     predicted_sentence = tokenizer.decode([i for i in prediction if i < tokenizer.vocab_size])
@@ -38,5 +8,3 @@ def predict(sentence):
     print('Output: {}'.format(predicted_sentence))
 
     return predicted_sentence
-
-
--- chatbot/mbti_chatbot/talkModel/talk_model.py
+++ chatbot/mbti_chatbot/talkModel/talk_model.py
@@ -27,13 +27,39 @@ NUM_HEADS = 8
 DFF = 512 #2048
 DROPOUT = 0.1
 
-def transformer_model():
-    tf.keras.backend.clear_session()
+tf.keras.backend.clear_session()
 
-    return transformer(
-        vocab_size=VOCAB_SIZE,
-        num_layers=NUM_LAYERS,
-        dff=DFF,
-        d_model=D_MODEL,
-        num_heads=NUM_HEADS,
-        dropout=DROPOUT)
+talk_model = transformer(
+    vocab_size=VOCAB_SIZE,
+    num_layers=NUM_LAYERS,
+    dff=DFF,
+    d_model=D_MODEL,
+    num_heads=NUM_HEADS,
+    dropout=DROPOUT)
+    
+talk_model.load_weights(model_weights_path)
+
+# 사용자 메세지 전처리
+def preprocess_sentence(sentence):
+    sentence = re.sub(r"([?.!,])", r" \1 ", sentence)
+    sentence = sentence.strip()
+    return sentence
+
+# 응답 메세지에 해당하는 단어 예측
+def evaluate(sentence):
+    sentence = preprocess_sentence(sentence)
+    sentence = tf.expand_dims(START_TOKEN + tokenizer.encode(sentence) + END_TOKEN, axis=0)
+
+    output = tf.expand_dims(START_TOKEN, 0)
+
+    for i in range(MAX_LENGTH):
+        predictions = talk_model(inputs=[sentence, output], training=False)
+        predictions = predictions[:, -1:, :]
+        predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
+
+        if tf.equal(predicted_id, END_TOKEN[0]):
+            break
+
+        output = tf.concat([output, predicted_id], axis=-1)
+        
+    return tf.squeeze(output, axis=0)
